5bea76d3fcb4c5c7a1abc17bebcf4372cb68d7e4
SENAI - ALUNO <SENAI - ALUNO@280101SENAI-SL0>
2025-02-27 19:26:41 -0300

    atualizando o arquivo para receber o codigo inferior ao final.

--- MQTT Samuel 1/Form1.cs
+++ MQTT Samuel 1/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using uPLibrary.Networking.M2Mqtt;
 
 namespace MQTT_Samuel_1
 {
@@ -16,5 +18,81 @@ namespace MQTT_Samuel_1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        delegate void SetTextCallback(string text);
+        private void SetText(string text)
+        {
+            if (this.txtEventos.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+                txtEventos.AppendText(text + "\r\n");
+        }
+
+        string mensagem;
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+
+            byte[] dado = e.Message;
+
+            mensagem = "";
+
+            for (int i = 0; i < dado.Length; i++)
+            {
+                mensagem += Convert.ToChar(dado[i]).ToString();
+            }
+
+            SetText(e.Topic + ":" + mensagem);
+
+        }
+
+
+        MqttClient cliente = null;
+     
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cliente = new MqttClient("broker.hivemq.com", 1883, false, MqttSslProtocols.None, null, null);
+
+                string clientId = Guid.NewGuid().ToString();
+
+                cliente.Connect(clientId);
+
+                cliente.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+                // Subscribe messages
+                cliente.Subscribe(new string[] { "SENAI/TOPICO_TESTE" },
+                    new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+                MessageBox.Show("Conectado com sucesso!", "SENAI", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "SENAI", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void FormClose(object sender, FormClosingEventArgs e)
+        {
+            cliente.Disconnect();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            cliente.Publish("SENAI/TESTE", ASCIIEncoding.UTF8.GetBytes(txtenviar.Text));
+        }
     }
 }
+
+    
